adb1846ee6a2384e871325f8940b3ccea1426652
doemeyer <36677554+doemeyer@users.noreply.github.com>
2026-05-12 11:18:57 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -337,7 +337,7 @@ function initialize() {
             return results[1] || 0;
         }
     };
-    const loadUrlDB = "examples/Beispieldatensatz_Kundendaten.db";
+    const loadUrlDB = "examples/pokedex.sqlite";
     if (loadUrlDB != null) {
         setIsLoading(true);
         const xhr = new XMLHttpRequest();
